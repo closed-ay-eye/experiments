f7641bd852ece5dc68d3b526275e1da7a45831b7
Thiago R <thigobr@yahoo.com.br>
2024-05-26 18:47:39 -0700

    Add Gemini vision detection code

---
+++ gemini.py
@@ -0,0 +1,22 @@
+import google.generativeai as genai
+import os
+import PIL.Image
+
+
+class Gemini:
+    def __init__(self, google_api_key):
+        genai.configure(api_key=google_api_key)
+        self.model = genai.GenerativeModel('gemini-pro-vision')
+
+    def detect_ingredients(self, ingredients_picture):
+        prompt = """This image contains ingredients for a food recipe. I would like you to identify all the ingredients
+        in this image and return their names as a comma separated list."""
+        return self.model.generate_content([prompt, ingredients_picture])
+
+
+if __name__ == "__main__":
+    api_key = os.getenv('GOOGLE_API_KEY')
+    gemini = Gemini(api_key)
+    image = PIL.Image.open('./images/shrimp-tofu.webp')
+    detection = gemini.detect_ingredients(image)
+    print(f"Ingredients detected:{detection.text}")
