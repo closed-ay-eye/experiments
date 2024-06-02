8d8016e41eb3073c3117a38b3c3173d5a18e61cb
Tony Dao <tdao407@gmail.com>
2024-06-02 00:58:14 -0700

    texttospeech for the steps

--- src/speech/texttospeech.py
+++ src/speech/texttospeech.py
@@ -1,11 +1,19 @@
-from langchain_community.llms import OpenAI
 from langchain_community.tools import ElevenLabsText2SpeechTool
 from crewai import Agent, Task, Crew, Process
 import os
 from langchain_core.tools import Tool
+import pandas as pd
+from pandas.core.series import Series
 
 # Replace your ELEVEN_API_KEY here
-os.environ["ELEVEN_API_KEY"] = ""
+# os.environ["ELEVEN_API_KEY"] = ""
+
+def _format_recipe(recipe: Series) -> str:
+    name = recipe['name']
+    ingredients = "\n".join((map(lambda x: f"- {x.replace('  ', '')}", eval(recipe['ingredients_raw_str']))))
+    steps = "\n".join((map(lambda x: f"- {x}", eval(recipe['steps']))))
+    return steps
+    # return dedent(f"""Name: {name}\nIngredients:\n{ingredients}\n\nSteps:\n{steps}\n""")
 
 class TextToSpeech:
     def __init__(self):
@@ -25,7 +33,9 @@ class TextToSpeech:
             tools=[self.__text2speech_tool],
             verbose=True
         )
-    def createSpeech(self, text):
+
+    def createSpeech(self, recipe: Series):
+        recipe = _format_recipe(recipe)
         task = Task(
             description="Given some {text}, convert it to speech",
             agent=self.__agent,
@@ -37,8 +47,10 @@ class TextToSpeech:
             process=Process.sequential,
             verbose=True,
         )
-        return crew.kickoff(inputs={"text": text})
+        return crew.kickoff(inputs={"text": recipe})
 
 if __name__ == "__main__":
+    df = pd.read_csv("../../dataset/recipes_w_search_terms.csv")
     t2S = TextToSpeech()
-    t2S.createSpeech("Hello World, this is testing text to speech")
+    # t2S.createSpeech("Hello World, this is testing text to speech")
+    t2S.createSpeech(df.iloc[0])
