d04eb91ab2f0f5e8d10bef24923eb4764d998615
André Oriani <aoriani@aoriani.dev>
2024-06-04 23:10:51 -0700

    google tts test

---
+++ src/googletts/tts.py
@@ -0,0 +1,28 @@
+if __name__ == "__main__":
+    """Synthesizes speech from the input string of text."""
+    from google.cloud import texttospeech
+
+    client = texttospeech.TextToSpeechClient()
+
+    input_text = texttospeech.SynthesisInput(text="My dear apprentice, gather the following ingredients  Aaaaaah! Delicious! Oh my god.")
+
+    # Note: the voice can also be specified by name.
+    # Names of voices can be retrieved with client.list_voices().
+    voice = texttospeech.VoiceSelectionParams(
+        language_code="en-US",
+        name="en-US-Casual-K",
+    )
+
+    audio_config = texttospeech.AudioConfig(
+        audio_encoding=texttospeech.AudioEncoding.LINEAR16,
+        speaking_rate=1
+    )
+
+    response = client.synthesize_speech(
+        request={"input": input_text, "voice": voice, "audio_config": audio_config}
+    )
+
+    # The response's audio_content is binary.
+    with open("output.mp3", "wb") as out:
+        out.write(response.audio_content)
+        print('Audio content written to file "output.mp3"')
